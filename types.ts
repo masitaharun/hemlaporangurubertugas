e86772d78bd9c7d435fd7655b5653d59e06b2cbe
masitaharun <g-80027491@moe-dl.edu.my>
2026-01-28 15:03:08 +0800

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,3 +1,4 @@
+
 export interface ReportData {
   minggu: string;
   tarikh: string;
@@ -57,12 +58,56 @@ export interface ReportData {
   tarikhLaporan: string;
 }
 
+/**
+ * SALIN DAN LEKAT SENARAI NAMA GURU ANDA DI SINI.
+ * Pastikan setiap nama diletakkan dalam pembuka dan penutup kata " " dan dipisahkan dengan koma.
+ */
+export const SENARAI_GURU_RASMI: string[] = [
+  "ZAHAREL BIN ATAN",
+  "KURSINA BINTI AYOB",
+  "SUHAILY BINTI SAMSUDIN",
+  "AZRUL NOR BIN FADZILAH",
+  "NORHADI SHAM BIN HAMZAH",
+  "SYLVIANO BIN ABU BAKAR",
+  "ZUL AZWAL BIN MOHAMED",
+  "MOHD RAZIF BIN FATHLAN",
+  "SYLVIANO BIN ABU BAKAR",
+  "MUHAMMAD HAKIM BIN MOHD SHARIF",
+  "MUHAMMAD IZZUDDIN BIN ABDUL WAHAB",
+  "SURIANI BINTI ANDI MAPPEASSE",
+  "NORIZAN BINTI MOHAMAD NOR",
+  "ROSLIZA BINTI ABD MANAP",
+  "SALMAH BINTI SALIMAN",
+  "NORHUDA BINTI ABD RAZAK",
+  "ALHANIM BINTI HAMZAH",
+  "ROSILAWATI BINTI GHAZALI",
+  "JUNAIDAH  BINTI YATIM",
+  "NURUL BALQIS BINTI RASHID@MOHAMAD",
+  "RUHAILAH BINTI MOHD SAFIAN",
+  "MASHITAH BINTI MOHD FAKRI",
+  "NORAIDAH BINTI MAT ZAIMIN",
+  "NOOR MASITAH BINTI HARUN",
+  "SYAFIKAH AINUN BINTI MOHAMMAD SABRI",
+  "NUR ATIRAH BINTI ZALAHUDDIN",
+  "NUR SYAKILA BINTI ZAHARLUDIN",
+  "SITI NUR ASMIRA BINTI NOR SHAH",
+  "SITI NAZIHA BINTI JOPRI",
+  "THARSHINIE A/P SUPPIAH",
+  "NUR AMIRAH BINTI MOHD ADENAN",
+  "FATIN NUR AMELIA BINTI AZIZON",
+  "NORLAILI BINTI MUHAMMAD ZAMBERI",
+  "NADIA NATASHA BINTI MHD ROSLAILE",
+  "NOR NAEZAH BINTI ABU TALIB",
+  "NUR AMIRAH BINTI SUHAIMI",
+  // Tambah lagi nama guru di sini mengikut keperluan sekolah anda
+].sort();
+
 export const INITIAL_DATA: ReportData = {
   minggu: '',
   tarikh: '',
   hari: '',
   namaGuru: Array(8).fill(''),
-  senaraiGuruMaster: [],
+  senaraiGuruMaster: SENARAI_GURU_RASMI, // Menggunakan senarai rasmi sebagai default
   perhimpunan: { isiUtama: '', komen: '' },
   kantin: { kebersihan: 'Baik', komenKebersihan: '', kualiti: 'Sederhana', komenKualiti: '' },
   tandas: { tahap: 'Memuaskan', komen: '' },
